c13f28b2fbfe787ed8486a983e352fdd115e304f
Ankan121 <ankanbiswas942>
2024-07-01 22:14:36 +0600

    GPA used IF-Else

---
+++ feugpa.dart
@@ -0,0 +1,18 @@
+void main() {
+  var Number = 80;
+  if (Number >= 90) {
+    print('Grate is =  A+');
+  } else if (Number >= 80) {
+    print('Grate is =  A');
+  } else if (Number >= 70) {
+    print('Grate is =  B');
+  } else if (Number >= 60) {
+    print('Grate is =  C');
+  } else if (Number >= 50) {
+    print('Grate is =  D');
+  } else if (Number >= 36) {
+    print('Grate is =  E');
+  } else {
+    print("Grate is = Fail");
+  }
+}
