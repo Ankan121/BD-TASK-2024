f70dad9af25928ec5cd7d094438283d768ef5b52
Ankan121 <ankanbiswas942@gmail.com>
2024-07-01 23:59:22 +0600

    Update fuasdm.dart

---
+++ Funcation/fuasdm.dart
@@ -0,0 +1,30 @@
+void main() {
+  sum();
+  muliplication();
+  minus();
+  divide();
+}
+
+sum() {
+  var a = 20;
+  var b = 30;
+  print('Sum is = ${a + b}');
+}
+
+muliplication() {
+  var a = 20;
+  var b = 30;
+  print('muliplication is = ${a * b}');
+}
+
+minus() {
+  var a = 40;
+  var b = 30;
+  print('Minus is = ${a - b}');
+}
+
+divide() {
+  var a = 20;
+  var b = 3;
+  print('Divide is = ${a / b}');
+}
