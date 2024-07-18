5039a1ffdbd653e70d41beb7b6a8a6b5b766ba61
Ankan Chandra Biswas <ankanbiswas942@gmail.com>
2024-07-18 16:07:23 +0600

    Update wlum.dart

--- Dart Class/multiplication-table/wlum.dart
+++ Dart Class/multiplication-table/wlum.dart
@@ -1,6 +1,6 @@
 void main() {
   var number = 1;
-  var multiplication = 10;
+  var multiplication = 1;
   while (number <= 10) {
     print("$multiplication × $number = ${number * 10}");
     number++;
